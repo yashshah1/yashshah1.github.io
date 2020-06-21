1e4d1135108b37ae23ffe94af2a6ea6978a829fd
Yash Shah <yashah1234@gmail.com>
2020-06-21 21:21:59 +0530

    Updates

---
+++ precache-manifest.b2fb0612ef658dc1b552a329028d5492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebdde077d7f48738889a31c4c2170af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36965d739626ae4a9a7c",
+    "url": "/static/css/main.4aad5461.chunk.css"
+  },
+  {
+    "revision": "bf6ef709b0e5a0688e2c",
+    "url": "/static/js/2.6fabd476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6fabd476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36965d739626ae4a9a7c",
+    "url": "/static/js/main.5f0c496a.chunk.js"
+  },
+  {
+    "revision": "77b7fea5572c627fed44",
+    "url": "/static/js/runtime-main.bd2298fb.js"
+  }
+]);
